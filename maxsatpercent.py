68f4b85fa3941eab2fdfa5d0c6531c534572c969
aubema <60398833+aubema@users.noreply.github.com>
2023-04-27 15:42:15 -0400

    Add files via upload

---
+++ maxsatpercent.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import os
+import sys
+import numpy as np
+
+user = "sand"
+path = "/home/" + user
+file = path + "/saturation.tmp"
+with open(file, "r") as f:
+    words = [word for line in f for word in line.split()]
+R = float(words[2])
+G = float(words[5])
+B = float(words[8])
+colors = [R, G, B]
+max = round(np.amax(colors) * 100)
+print(max)
