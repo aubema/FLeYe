c41f2d094a6ef4ae8679ee77e7047711ac3ff29d
aubema <martin.aube@cegepsherbrooke.qc.ca>
2023-05-02 23:57:52 +0000

    integrating sunset

--- observe_fleye.bash
+++ observe_fleye.bash
@@ -56,21 +56,55 @@ nightg=16		# ISO 1600
 user="sand"
 gain=$nightg
 ta=$nightt
-sunset="2023-01-29T19:00:00 UTC"
-sunrise="2023-01-30T06:00:00 UTC"
-sset=`date -d "$sunset" +%s`
-srise=`date -d "$sunrise" +%s`
 cams=(A B C D)
 basepath="/var/www/html/data"
 backpath="/home/"$user"/data"
 path="/home/"$user
 configfile=$path/FLeYe_RPI_1_config
+generalconfig=$path/FLeYe_general_config
+# determine sunrise and sunset
+grep "Delay2UTC" $generalconfig > $path/generaltmp
+read bidon bidon DUTC bidon < $path/generaltmp
+grep "Latitude" $generalconfig > $path/generaltmp
+read bidon bidon LAT bidon < $path/generaltmp
+grep "Longitude" $generalconfig > $path/generaltmp
+read bidon bidon LON bidon < $path/generaltmp
+
+
+/usr/bin/hdate -s -l $LAT -L $LON -z -$DUTC | grep sunrise > $path/suntmp
+read bidon value bidon < $path/suntmp
+
+
+echo "rise" $value
+
+sunrise=`date --date="$value $DUTC hours" "+%Y-%m-%dT%H:%M:%S UTC"`
+/usr/bin/hdate -s -l $LAT -L $LON -z -$DUTC | grep sunset > $path/suntmp
+read bidon value bidon < $path/suntmp
+
+
+echo "set" $value
+
+
+sunset=`date --date="$value $DUTC hours" "+%Y-%m-%dT%H:%M:%S UTC"`
+echo "Sunset= " $sunset
+echo "Sunrise= " $sunrise
+# convert sunset and sunrise in seconds today, yesterday and tomorrow (approximate values)
+sset=`date -d "$sunset" +%s`
+srise=`date -d "$sunrise" +%s`
+now=`date +%s`
+let ssetbefore=sset-86400
+let ssetafter=sset+86400
+let srisebefore=srise-86400
+let sriseafter=srise+86400
+
+
 yy=`date +%Y`
 mo=`date +%m`
 dd=`date +%d`
 hh=`date +%H`
 mm=`date +%M`
 mm=${mm##+(0)}		# remove leading 0
+if [ "$mm" == "09" ]; then let mm=9; fi   # seem to be a bug with 09???
 let nextmm=mm+1
 if [ $nextmm -lt 10 ]
 then nextmm="0"$nextmm
@@ -93,21 +127,23 @@ while :
 do 	time1=`date +%s`
 	tail -1 $path/image_list.txt > $path/seq_num.tmp 
 	read secnum bidon < $path/seq_num.tmp
-	if [ $time1 -lt $sset ] || [ $time1 -ge $srise ]
+	if [[ $time1 -lt $sset  &&  $time1 -ge $srise ]] || [[ $time1 -lt $ssetbefore  &&  $time1 -ge $srisebefore ]] || [[ $time1 -lt $ssetafter &&  $time1 -ge $sriseafter ]]
 	then 	echo "day"
 		ta=$dayt
 		gain=$dayg
+		echo "You are observing during daytime"
 	else	echo "night"
 		ta=$nightt
 		gain=$nightg
+		echo "You are observig during nighttime"
 	fi
 	echo "Shooting..."
 	let n=0
 	for cam in ${cams[@]}
-	do 	grep "Lens"$cam $configfile > lenstmp
-		read bidon bidon lens bidon < lenstmp
-		grep "Posi"$cam $configfile> positmp
-		read bidon bidon posi bidon < positmp
+	do 	grep "Lens"$cam $configfile > $path/lenstmp
+		read bidon bidon lens bidon < $path/lenstmp
+		grep "Posi"$cam $configfile> $path/positmp
+		read bidon bidon posi bidon < $path/positmp
 		take_pictures "$cam" "$gain" "$ta"
    		yy=`date +%Y`
    		mo=`date +%m`
@@ -130,19 +166,22 @@ do 	time1=`date +%s`
 		then 	/bin/mkdir $backpath/$yy/$mo
 		fi
 		echo "=============================="
-		# rename pictures
+		# renaming pictures
 		cp -f $path"/capture_"$cam".dng" $basepath/$yy/$mo/$image"_"$secnum".dng"
 	    	mv -f $path"/capture_"$cam".dng" $backpath/$yy/$mo/$image"_"$secnum".dng"
 		cp -f $path"/capture_"$cam".jpg" $basepath/$yy/$mo/$image"_"$secnum".jpg"
 	    	mv -f $path"/capture_"$cam".jpg" $backpath/$yy/$mo/$image"_"$secnum".jpg"
 		let n=n+1
 	done
+	# flush ram cache to correct a memory leak in the camera library
 	sync
 	echo 3 > /proc/sys/vm/drop_caches
 	let secnum=secnum+1
+	# write data to the image_list.txt file
 	echo $secnum ${image_list[@]} >> $path/image_list.txt
 	cp -f $path"/image_list.txt" $basepath/$yy/$mo/
 	cp -f $path"/image_list.txt" $backpath/$yy/$mo/
+	# calculate waiting time until next shooting
 	time2=`date +%s`
 	let idle=60-time2+time1  # one measurement every 60 sec 
 	if [ $idle -lt 0 ]
