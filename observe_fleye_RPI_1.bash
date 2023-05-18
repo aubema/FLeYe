aad569b04cba744bb0b263281f9e8b8e7e3f5202
Martin Aube <martin.aube@cegepsherbrooke.qc.ca>
2023-05-18 14:15:38 -0400

    ntpdate fix

--- observe_fleye_RPI_1.bash
+++ observe_fleye_RPI_1.bash
@@ -86,7 +86,7 @@ generalconfig=$path/FLeYe_general_config
 # start gps
 sudo gpsd /dev/serial0 -F /var/run/gpsd.sock
 # sync time
-ntpdate 172.20.4.230   # SET THE RIGHT IP HERE: MASTER IP FOR THE SLAVE AND GONDOLA NTP IP FOR THE MASTER
+/usr/sbin/ntpdate 172.20.4.230   # SET THE RIGHT IP HERE: MASTER IP FOR THE SLAVE AND GONDOLA NTP IP FOR THE MASTER
 syncflag=`exit $?`
 if [ "$syncflag" == "0"]
 then 	echo "Time has synced"
@@ -154,7 +154,7 @@ do 	time1=`/usr/bin/date +%s`
 		read bidon bidon posi bidon < $path/positmp
 		take_pictures "$cam" "$gain" "$ta"
 		# reading gps position
-		globalpos
+		#globalpos
 		gps_list[$n]=$lat"_"$lon"_"$alt
    		yy=`/usr/bin/date +%Y`
    		mo=`/usr/bin/date +%m`
